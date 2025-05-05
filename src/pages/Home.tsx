dd1766416c8b41f4fe94c6765e080d01210daca4
Xavanion <barrettbrown2012@gmail.com>
2025-05-05 14:23:54 -0500

    Finished home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,37 @@
+import git_logo from "../assets/git_logo.png";
+import linkedin_logo from "../assets/linkedin_logo.png";
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <div className="mx-100 mt-25">
       <h1>&lt;Barrett Brown/&gt;</h1>
+      <p className="ml-10 mt-5 font-fira font-[20px]">
+        Backend-focused developer building scalable systems and writing about
+        the problems I create and solve along the way
+      </p>
+      <div className="flex flex-row gap-5 ml-10 mt-5">
+        <Link
+          to="https://github.com/Xavanion"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cursor-pointer transition-transform hover:scale-110 focus:scale-110"
+        >
+          <img src={git_logo} alt="github logo" className="h-[2.3rem] w-auto" />
+        </Link>
+        <Link
+          to="www.linkedin.com/in/barrett-brown-8a990016b"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cursor-pointer transition-transform hover:scale-110 focus:scale-110"
+        >
+          <img
+            src={linkedin_logo}
+            alt="linkedin logo"
+            className="h-[2.3rem] w-auto"
+          />
+        </Link>
+      </div>
     </div>
   );
 }
