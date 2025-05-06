c67109486a2baf7e3729a163ae642d6d48072fb0
Xavanion <barrettbrown2012@gmail.com>
2025-05-06 12:08:49 -0500

    Finished project display

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -8,18 +8,24 @@ import { Link } from "react-router-dom";
 function ProjectCard({ p }: { p: ProjectInfo }) {
   return (
     <div className="flex flex-col mt-20">
-      <h2>{p.title}</h2>
-      <Link
-        to={p.repo}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="cursor-pointer transition-transform hover:scale-110 focus:scale-110"
-      >
-        <img src={logo} alt="repo logo" className="ml-5 h-[2.3rem] w-auto" />
-      </Link>
+      <div className="flex flex-row">
+        <h2 className="mr-5">{p.title}</h2>
+        <Link
+          to={p.repo}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cursor-pointer transition-transform hover:scale-110 focus:scale-110"
+        >
+          <img src={logo} alt="repo logo" className="h-[2.3rem] w-auto" />
+        </Link>
+      </div>
       <p className="font-fira font-medium">{p.description}</p>
       {/* TODO: Create a mapped roadmap and make each show as a bullet point */}
-      <p className="font-fira font-medium gap-2">{p.roadmap}</p>
+      <ul className="mt-3 list-disc list-inside space-y-1 font-medium font-fira">
+        {p.roadmap.map((goal) => (
+          <li>{goal}</li>
+        ))}
+      </ul>
     </div>
   );
 }
